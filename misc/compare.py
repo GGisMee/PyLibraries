38c70e226e018fbd295080fd7e20df49b6d76ff2
GGisMee <gustav.gamstedt@outlook.com>
2023-08-11 23:29:27 +0200

    Replaced ml_toolkit

---
+++ misc/compare.py
@@ -0,0 +1,6 @@
+str1 = input('Input string one')
+str2 = input('Input string two')
+str1 = 'https://raw.githubusercontent.com/GGisMee/PyLibraries/main/ml_toolkit/helper_functions.py'
+str2 = 'https://raw.githubusercontent.com/GGisMee/PyLibraries/ml_toolkit/helper_functions.py'
+if len(str1) != len(str2):
+    print(f'Length of: str 1: {len(str1), len(str2)}')
